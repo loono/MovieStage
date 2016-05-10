2d184f4d617a5097ec20cf429b1943a1cad498c3
loono <linnanthu@163.com>
2016-05-09 22:48:21 -0700

    Setup

---
+++ app/src/main/java/com/loono/android/moviestage/MoviePostersActivity.java
@@ -0,0 +1,13 @@
+package com.loono.android.moviestage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoviePostersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_posters);
+    }
+}
